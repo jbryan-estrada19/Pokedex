bd15c5e6628a9b3977bbb704b30c2661f56f1842
Estrada <jberap.cas.est619@gmail.com>
2023-10-04 19:38:11 +0800

    finished working prototype

--- pokebelt.py
+++ pokebelt.py
@@ -2,25 +2,49 @@ import streamlit as st
 import functions as fn
 import pokedex as dex
 
+
 pokebelt = fn.get_pokemon()
 
 def add_pokemon():
-    pokemon = st.session_state['add_pokemon'] + "\n"
+    pokemon = st.session_state['new_pokemon'] + "\n"
     pokebelt.append(pokemon)
     print(pokemon)
     fn.write_pokemon(pokebelt)
 
-
-
-
-for pokemon in pokebelt:
-    st.subheader(pokemon)
-    st.write(dex.search_pokemon(pokemon))
-    #st.columns() #Choose possible/learnable attacks
-    #st.write() #Held item 
+# def add_item():
+#     item = st.session_state['new_item'] + "\n"
+    
 
 st.title("My Pokemon Team")
-
 st.text_input(label = "Add new pokemon", placeholder="Who's that Pokemon?", 
               on_change=add_pokemon, key='new_pokemon')
+
+stripped_pokebelt = []
+for i in pokebelt:
+    stripped_pokebelt.append(i.strip())
+
+for pokemon in stripped_pokebelt:
+    col1, col2 = st.columns(2)    
+
+    with col1:
+        st.subheader(pokemon.capitalize())
+        pokemon_details = dex.search_pokemon(pokemon)
+        image = f"https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/{pokemon_details[0]}.svg"
+        st.image(image, width = 200 )
+        # st.text_input(label = "Held item", placeholder="Which item?", 
+        #       on_change=add_item, key='new_item')
+
+    with col2:
+        st.write(f"""\n
+                Pokemon: {pokemon_details[1]} \n
+                ID: {pokemon_details[0]} \n
+                Base HP: {pokemon_details[2]} \n
+                Base Attack: {pokemon_details[3]} \n
+                Base Defense: {pokemon_details[4]} \n
+                Base Special Attack: {pokemon_details[5]} \n
+                Base Special Defense: {pokemon_details[6]} \n
+                Base Speed: {pokemon_details[7]}\n
+                \n ________________
+                
+                """)
 
--- pokedex.py
+++ pokedex.py
@@ -1,23 +1,42 @@
 import requests as rq
+import streamlit as st
+
+@st.cache_data
 
 
 def search_pokemon(name):
     response = rq.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
-    pokemon = response.json()
-    poke_name = pokemon["name"]
-    poke_id = pokemon["id"]
-    base_stat = pokemon["stats"]
-    base_hp = (base_stat[0])['base_stat']
-    base_atk =(base_stat[1])['base_stat']
-    base_def = (base_stat[2])['base_stat']
-    base_satk = (base_stat[3])['base_stat']
-    base_sdef = (base_stat[4])['base_stat']
-    base_spd = (base_stat[5])['base_stat']
-    poke_stats = [poke_id,poke_name,base_hp,base_atk,base_def,base_satk,base_sdef,base_spd]
-    return poke_stats
+    if response.status_code == 200:
+        try:
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            searched_pokemon = response.json()
+            poke_name = searched_pokemon["name"]
+            poke_id = searched_pokemon["id"]
+            base_stat = searched_pokemon["stats"]
+            base_hp = base_stat[0]["base_stat"]
+            base_atk = base_stat[1]["base_stat"]
+            base_def = base_stat[2]["base_stat"]
+            base_satk = base_stat[3]["base_stat"]
+            base_sdef = base_stat[4]["base_stat"]
+            base_spd = base_stat[5]["base_stat"]
+            poke_stats = [poke_id, poke_name, base_hp, base_atk, base_def, base_satk, base_sdef, base_spd]
+            return poke_stats
+        except rq.exceptions.HTTPError as e:
+            print(f"Error: {e}")
+            return None  # Return None if the Pokemon is not found or there's an HTTP error
+        except rq.exceptions.RequestException as e:
+            print(f"Request Exception: {e}")
+            return None  # Handle other request exceptions gracefully
+        except ValueError as e:
+            print(f"JSON Decode Error: {e}")
+            return None  # Handle JSON decode errors gracefully
 
 
-    
+
+
+
+
+
 
 #https://pokeapi.co/docs/v2#pokemon-section
 
